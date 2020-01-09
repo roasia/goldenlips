0ee3d80cb2fb1decba6f2b0c421b78fc459b7903
juliawarakomska <juliawarakomska@github.com>
2020-01-09 12:15:25 +0100

    Form

---
+++ src/components/etap_1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="form">
+        <h1>Wyślij swoją propozycję tematu</h1>
+        <p>Zbieramy propozycje tematów na najbliższą edycję konkursu. Jeśli chcesz, możesz dodać swoją propozycję poniżej:</p>
+        <form action="julia.warakomska1a@gmail.com" method="post">
+	        <input type="text" name="wpisz_temat">Wpisz swoją propozycję...       
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
