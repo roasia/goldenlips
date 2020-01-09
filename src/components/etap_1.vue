dd62ef3cc570328f186a253be9d12763ef5bc2c6
juliawarakomska <juliawarakomska@github.com>
2020-01-09 14:44:17 +0100

    Form_3

--- src/components/etap_1.vue
+++ src/components/etap_1.vue
@@ -1,4 +1,5 @@
 <template>
+    
     <div id="form">
         <h1>Wyślij swoją propozycję tematu</h1>
         <p>Zbieramy propozycje tematów na najbliższą edycję konkursu. Jeśli chcesz, możesz dodać swoją propozycję poniżej:</p>
@@ -15,6 +16,46 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+    #etap_1{
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+        font-family: "Lato";
+        text-align:center;
+        text-transform:uppercase;
+        }
+
+    h1 {
+        font-size: 4em;
+        color:#f4c621;
+    }
+
+    p {
+        font-family: "Lato";
+        text-align:center;
+        color:#aeaeae;
+        font-size:2.5em;
+    }
+
+    form {
+        font-family: "Lato";
+        text-align:center;
+        color:#aeaeae;
+    }
+    #wpis {
+        color:#f4c621;
+        font-size:2em;
+        outline:none;
+        width:25em;
+        height:2.1em;
+        border:none;
+    }
+
+    input {
+        font-family: "Lato";
+        text-align:center;
+    }
 </style>
